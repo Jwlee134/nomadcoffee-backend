9a1d55d7596653003c50eb1554548d39e683d4dc
Jaewon <sorhd134@gmail.com>
2022-04-07 15:51:07 +0900

    Build and Start

--- src/schema.ts
+++ src/schema.ts
@@ -2,9 +2,11 @@ import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
 import path from "path";
 
-const loadedTypes = loadFilesSync(path.join(__dirname, "/**/*.typeDefs.ts"));
+const loadedTypes = loadFilesSync(
+  path.join(__dirname, "/**/*.typeDefs.{js,ts}")
+);
 const loadedResolvers = loadFilesSync(
-  path.join(__dirname, "/**/*.resolvers.ts")
+  path.join(__dirname, "/**/*.resolvers.{js,ts}")
 );
 
 export const typeDefs = mergeTypeDefs(loadedTypes);
