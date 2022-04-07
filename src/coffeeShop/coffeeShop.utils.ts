06fb2cda63d2a485664d6c35e1cce4f698e05b4d
Jaewon <sorhd134@gmail.com>
2022-04-07 15:12:34 +0900

    Slug

--- src/coffeeShop/coffeeShop.utils.ts
+++ src/coffeeShop/coffeeShop.utils.ts
@@ -2,7 +2,10 @@ export const processCategories = (categories: string[]) => {
   return (
     categories?.map((category: string) => ({
       where: { name: category },
-      create: { name: category },
+      create: {
+        name: category,
+        slug: category.toLowerCase().replace(/\s/g, "-"),
+      },
     })) || []
   );
 };
