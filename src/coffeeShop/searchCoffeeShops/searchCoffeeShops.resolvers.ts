fc4cb5a4e9fef95a36a288889ce9d22f76e106fd
Jaewon <sorhd134@gmail.com>
2022-04-28 20:37:07 +0900

    Feat: SearchCoffeeShops resolver

--- src/coffeeShop/searchCoffeeShops/searchCoffeeShops.resolvers.ts
+++ src/coffeeShop/searchCoffeeShops/searchCoffeeShops.resolvers.ts
@@ -2,10 +2,18 @@ import { Resolvers } from "../../types";
 
 const resolvers: Resolvers = {
   Query: {
-    seeCoffeeShops: (_, { lastId }, { client }) =>
+    searchCoffeeShops: (_, { lastId, keyword }, { client }) =>
       client.coffeeShop.findMany({
         take: 5,
         ...(lastId && { skip: 1, cursor: { id: lastId } }),
+        ...(keyword && {
+          where: {
+            OR: [
+              { categories: { some: { name: keyword } } },
+              { name: { startsWith: keyword, mode: "insensitive" } },
+            ],
+          },
+        }),
         include: {
           photos: true,
           user: { select: { username: true, avatarUrl: true } },
